6db69becc244865822ba99d5eaa0aabc006b1cd0
Loic Coyle <loic.coyle@hotmail.fr>
2022-01-27 23:11:26 +0100

    deps: cleanup

--- src/styles.rs
+++ src/styles.rs
@@ -214,6 +214,7 @@ mod tests {
     #[test]
     fn test_style() {
         let test_dir = get_data_dir().unwrap();
+        init(&test_dir).unwrap();
         let style = Style::new(test_dir.clone().join("default"), None).unwrap();
         assert_eq!(style.base_path, test_dir.join("default"));
         assert!(style.template_options.is_none());
